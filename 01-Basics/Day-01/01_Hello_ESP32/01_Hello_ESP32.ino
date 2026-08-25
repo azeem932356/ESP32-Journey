a7b00020caa3ed486d716c9650e8506c4866e9e6
Azeem Basha <Shaikazeem911030@gmail.com>
2026-08-25 20:47:19 +0530

    ESP32 Hello!

---
+++ 01-Basics/Day-01/01_Hello_ESP32/01_Hello_ESP32.ino
@@ -0,0 +1,13 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+
+Serial.println("Hello From ESP32!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
